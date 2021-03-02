f80d0545df5d0edc8553740d1a20d9531f355473
花 | Nadine's <77990703+RysepEchizen@users.noreply.github.com>
2021-03-02 14:45:50 +0700

    Update kernel.sh

--- kernel.sh
+++ kernel.sh
@@ -46,7 +46,7 @@ DEVICE="merlin"
 
 # The defconfig which should be used. Get it from config.gz from
 # your device or check source
-DEFCONFIG=merlin-perf_defconfig
+DEFCONFIG=merlin_defconfig
 
 # Specify compiler. 
 # 'clang' or 'gcc'
